e04eb24fc1b5d613587c5fa33127a51ba8dc0d45
baobao <thebbfrog@gmail.com>
2019-10-27 12:09:28 -0400

    progress

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,24 +3,43 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button goButton;
-    public void start (View view) {
+    ArrayList<Integer> answers = new ArrayList<>();
+    int locationOfCorrectAnswer;
+
+    public void start(View view) {
         goButton.setVisibility(View.INVISIBLE);
     }
+
+    public void chooseAnswer(View view){
+        if (Integer.toString(locationOfCorrectAnswer).equals((view.getTag().toString()))){
+            Log.i("Correct!","You got it" );
+        } else {
+            Log.i("Worng!", ":/");
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView sumTextView = findViewById(R.id.sumTextView4);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
 
         goButton = findViewById(R.id.gobutton);
 
@@ -29,5 +48,24 @@ public class MainActivity extends AppCompatActivity {
         int b = rand.nextInt(21);
 
         sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        for (int i = 0; i < 4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a + b);
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+                while (wrongAnswer == a + b){
+
+                    wrongAnswer = rand.nextInt(41);
+                }
+                answers.add(wrongAnswer);
+            }
+        }
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
